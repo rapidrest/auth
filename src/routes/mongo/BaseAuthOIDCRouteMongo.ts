76b1b920ee99b2b895952c115e3a4ee456aaf3e9
Jean-Philippe Steinmetz <caskater47@gmail.com>
2026-08-22 17:47:13 -0700

    Removign unused imports

--- src/routes/mongo/BaseAuthOIDCRouteMongo.ts
+++ src/routes/mongo/BaseAuthOIDCRouteMongo.ts
@@ -2,7 +2,6 @@
 // Copyright (C) 2026 Jean-Philippe Steinmetz. All rights reserved.
 // SPDX-License-Identifier: MPL-2.0
 ///////////////////////////////////////////////////////////////////////////////
-import { OIDCProvider } from "../../auth/OIDCStrategy.js";
 import { AliasMongo, ProfileMongo, UserMongo } from "../../mongo.js";
 import { BaseAuthOIDCRoute } from "../BaseAuthOIDCRoute.js";
 
--- src/routes/sql/BaseAuthOIDCRouteSQL.ts
+++ src/routes/sql/BaseAuthOIDCRouteSQL.ts
@@ -2,7 +2,6 @@
 // Copyright (C) 2026 Jean-Philippe Steinmetz. All rights reserved.
 // SPDX-License-Identifier: MPL-2.0
 ///////////////////////////////////////////////////////////////////////////////
-import { OIDCProvider } from "../../auth/OIDCStrategy.js";
 import { AliasSQL, ProfileSQL, UserSQL } from "../../sql.js";
 import { BaseAuthOIDCRoute } from "../BaseAuthOIDCRoute.js";
 
